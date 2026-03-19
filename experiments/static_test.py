88e67cff2b8b69264e9c2d831fee4190d7e08825
Nikhitha747 <gvreddy42757@gmail.com>
2026-03-19 19:33:53 +0530

    Update static_test.py

--- experiments/static_test.py
+++ experiments/static_test.py
@@ -1,14 +1,20 @@
 from src.grid import generate_grid
-from src.astar import astar
+from src.astar import astar, get_path
+import time
 
-def run():
-    grid = generate_grid(20, 0.2)
+densities = [0.1, 0.25, 0.4]
+
+for d in densities:
+    grid = generate_grid(70, d)
     start = (0, 0)
-    goal = (19, 19)
+    goal = (69, 69)
 
+    t1 = time.time()
     parent = astar(grid, start, goal)
+    t2 = time.time()
 
-    print("Static Path Planning Done")
+    path = get_path(parent, goal)
 
-if __name__ == "__main__":
-    run()
+    print("\nDensity:", d)
+    print("Path Length:", len(path))
+    print("Time:", t2 - t1)
